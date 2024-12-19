7bd192d0aa378e6841525d222cec685b8441e97d
Sandeep Gautam <145046164+Sandeep-Gautam83@users.noreply.github.com>
2024-12-19 12:10:16 +0000

    dataypes summary done

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -8,3 +8,28 @@
 // Reference Type ( Non primitive)
 
 // Array, Objects, Functions 
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory type 2   1. Stack (Primitive data type used hota hai)   2. Heap (Non primitive data type used hota hai)
+
+let myYoutudename = "Sandeepdotcom"
+let anothername = myYoutudename
+anothername="code with learn "
+
+console.log(myYoutudename);
+console.log(anothername);
+
+
+let userOne  = {
+    email: "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email="Sandeep@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
