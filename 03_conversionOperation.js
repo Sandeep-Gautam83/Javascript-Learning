ff55a2b586ce9a5fe5f0693862331807fe36674b
Sandeep Gautam <145046164+Sandeep-Gautam83@users.noreply.github.com>
2024-12-19 10:09:59 +0000

    container config done

---
+++ 03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+// true = 1, false = 0
+
+
+let isLpoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLpoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
