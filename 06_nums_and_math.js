a63db31b0ca4e1edfa2ee88080f342319a0ab9ea
Sandeep Gautam <145046164+Sandeep-Gautam83@users.noreply.github.com>
2024-12-19 13:23:50 +0000

    math and num

---
+++ 06_nums_and_math.js
@@ -0,0 +1,21 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(500);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const hunderds = 100000034
+console.log(hunderds.toLocaleString());
+
+// ++++++++++++++ MATHS +++++++++++++++++++
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.max(4,5,7,8,6));
+console.log(Math.min(9,5,5,43,2,4,56,4));
+
+console.log(Math.random());
