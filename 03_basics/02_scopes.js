165c9d3bee9183a5c1cd0d162d9d7175bb7383b1
Sandeep Gautam <145046164+Sandeep-Gautam83@users.noreply.github.com>
2024-12-20 13:19:06 +0000

    scopesclass config done

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,33 @@
+/*
+let a = 20
+const b=23
+var c = 243
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+*/
+
+if(true) {
+    let a = 20
+const b=23
+var c = 243
+
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+let a= 300
+if(true) {
+    let a=12
+    const b= 24
+    console.log("INNER : ", a);
+}
+console.log(a);
+
+
+
+
